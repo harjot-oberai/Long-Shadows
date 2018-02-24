5f725223b2ceb70638f43add8c45f43f91882042
harjot-oberai <harjot.oberai@gmail.com>
2018-02-24 22:02:40 +0530

    Adds testing layout and assets

--- app/src/main/java/com/sdsmdg/harjot/longshadowsandroid/MainActivity.java
+++ app/src/main/java/com/sdsmdg/harjot/longshadowsandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package com.sdsmdg.harjot.longshadowsandroid;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Typeface typeface = Typeface.createFromAsset(getResources().getAssets(), "fonts/Quicksand-Regular.ttf");
+
+//        ((TextView) findViewById(R.id.text_1)).setTypeface(typeface);
+//        ((TextView) findViewById(R.id.text_2)).setTypeface(typeface);
+//        ((TextView) findViewById(R.id.text_view_3)).setTypeface(typeface);
+//        ((TextView) findViewById(R.id.text_4)).setTypeface(typeface);
+
     }
 
 }
